11d83d88d52376d8f3f0f47c0d38d0e71dba45b0
dsdojo <dsdojo@hotmail.com>
2015-04-13 13:39:52 -0700

    updated asa queries and iot questions

--- Hack_Day/Test_Data_And_Sample_Outputs/StreamQL Repository.sql
+++ Hack_Day/Test_Data_And_Sample_Outputs/StreamQL Repository.sql
@@ -1,13 +1,24 @@
 ---------------------------------------------------------------------------------------------------
--- Return all the contents of the event hub once.
+--Return all the contents stored within the Event Hub and all the contents that will ever go 
+-- through the event hub.
 SELECT
     *
-INTO
-    YourOutput
 FROM
     MyEventHubStream
-TIMESTAMP BY
-    time
+
+-- Return all readings from the stream where humidity was higher than 70.
+select *
+FROM
+    MyInputAlias
+Where hmdt > 70
+
+-- Return all readings from streams where the humidity was higher than 70 or the temperature was 
+-- higher than 80, indefinitely from the past and future.
+select *
+FROM
+    MyInputAlias
+Where hmdt > 70
+OR temp > 80
 
 -- Return the average temperature every 3 seconds, from past and future readings indefinitely.
 SELECT
@@ -74,6 +85,7 @@ TIMESTAMP BY
 GROUP BY
     TumblingWindow(second, 3)
 
+
 -- Combine the last two queries about descriptive statistics together into one query.
 SELECT
     System.Timestamp AS WindowEnd,
@@ -109,12 +121,67 @@ TIMESTAMP BY
     time
 group by tumblingwindow(second, 3)
 
+-- Return min temperature, max temperature, and the change in temperature between min and max 
+-- of 3 second tumbling windows.
+SELECT
+    System.Timestamp as WindowEnd,
+    min(temp) as MinTemp,
+    max(temp) as MaxTemp,
+    max(temp) - min(temp) as DeltaTemp
+FROM
+    MyInputAlias Timestamp by time
+Group by TumblingWindow(second, 3)
+
+-- Write a query that returns the min and max time for events inside of 3 second tumbling windows
+-- and report the difference between the min and max time in seconds. Hint: datediff()
+SELECT
+    System.Timestamp as WindowEnd,
+    min(time) as MinTime,
+    max(time) as MaxTime,
+    max(time) - min(time) as DeltaTime
+FROM
+    MyInputAlias Timestamp by time
+Group by TumblingWindow(second, 3)
+
+------- Scenarios -------
+You own a fleet of dairy delivery trucks whose internal freezer temperatures need to be 
+maintained at 22° to 24°. How would you write a query that returns the status update every 5
+minutes for freezers who deviated from this range.
+SELECT
+    System.Timestamp AS WindowEnd,
+    dspl AS SensorName,
+FROM
+    MyInputAlias Timestamp by time
+GROUP BY
+    TumblingWindow(minute, 5), dspl
+HAVING
+    avg(temp) < 22 or avg(temp) > 24
+
+-- You’re in charge of 12 temperature and humidity controlled bio-domes in 
+-- Eastern Washington that grow pineapples. Pineapples need humidity between 
+-- 50% and 70% and temperature between 68°F and 86°F. If temperatures went below
+-- 68°F, plant growth will slow. Write a query that would notify you every time 
+-- either the humidity or temperature deviated out of the optimal range. 
+-- Hint: OR clause. 
+SELECT
+    System.Timestamp AS WindowEnd,
+    dspl AS SensorName,
+FROM
+    MyInputAlias Timestamp by time
+GROUP BY
+    TumblingWindow(minute, 5), dspl
+HAVING
+    avg(hmdt) < 50 
+    or avg(hmdt) > 70
+    or avg(temp) < 68
+    or avg(temp) > 86
+
 -------------------------------------------------------------------------------------------------------------
 
 -- Creates the humidity table
 CREATE TABLE humidity(
-	WindowEnd DATETIME2,
-	Humidity decimal(5,2)
+    WindowEnd DATETIME2,
+    Humidity decimal(5,2)
 )
 -- Creating a clustered index for humidity
 IF (NOT EXISTS (SELECT * FROM SYS.INDEXES 
@@ -138,10 +205,11 @@ TIMESTAMP BY
 Group by 
     HoppingWindow(second, 3, 1);
 
+
 -- Creates the humidity table
 CREATE TABLE sensor(
-	WindowEnd DATETIME2,
-	Humidity decimal(5,2)
+    WindowEnd DATETIME2,
+    Humidity decimal(5,2)
 )
 -- Creating a clustered index for humidity
 IF (NOT EXISTS (SELECT * FROM SYS.INDEXES 
@@ -165,19 +233,6 @@ TIMESTAMP BY
 Group by 
     HoppingWindow(second, 3, 1);
 
--------------------------------------------------------------------------------------
--- Vertically joining multiple streams and their difference in
--- humidity and temperature
-select
-    System.timestamp as "WindowEnd"
-    , sup.hmdt as SupHmdt, sup.temp as SupTemp
-    , bat.hmdt as BatHmdt, bat.temp as BatTemp
-    , (sup.hmdt - bat.hmdt) as HmdtDiff
-    , (sup.temp - sup.temp) as TempDiff
-from superman as sup timestamp by time
-join batman as bat timestamp by time
-on (datediff(second, sup, bat) between 0 and 1) and sup.time = bat.time
-where ((sup.hmdt - bat.hmdt) + (sup.temp - sup.temp)) > 0
 
 -------------------------------------------------------------------------------------
 -- Room Aggregations
