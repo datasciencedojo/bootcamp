31f64779a9b5eb3abdb229b530dcbbd3c7f311e5
Yuhui <yuhuizhang87@gmail.com>
2015-04-29 08:57:49 -0700

    add this new structure for test

---
+++ bootcamp_new_structure/Hack_Day/Samples/Setting_Up_ SQL_DB_Tables_Template.sql
@@ -0,0 +1,75 @@
+-------------------------------------------------------------------------------------------------------------
+
+-- Creates the humidity table
+CREATE TABLE humidity(
+    WindowEnd DATETIME2,
+    Humidity decimal(5,2)
+)
+-- Creating a clustered index for humidity
+IF (NOT EXISTS (SELECT * FROM SYS.INDEXES 
+                 WHERE NAME = 'IX_humidity_WindowEnd'))
+BEGIN
+CREATE CLUSTERED INDEX [IX_humidity_WindowEnd]
+    ON [dbo].[humidity]([WindowEnd] ASC);
+END
+
+--Open up a hoping window every second and calculate the 
+--moving average humidity for the past 3 seconds.
+SELECT
+    System.Timestamp as WindowEnd,
+    avg(hmdt) as Humidity
+INTO
+    HumidityTable
+FROM
+    streamsensor
+TIMESTAMP BY
+    time
+Group by 
+    HoppingWindow(second, 3, 1);
+
+
+-- Creates the humidity table
+CREATE TABLE sensor(
+    WindowEnd DATETIME2,
+    Humidity decimal(5,2)
+)
+-- Creating a clustered index for humidity
+IF (NOT EXISTS (SELECT * FROM SYS.INDEXES 
+                 WHERE NAME = 'IX_humidity_StartTime'))
+BEGIN
+CREATE CLUSTERED INDEX [IX_humidity_StartTime]
+    ON [dbo].[humidity]([time] ASC);
+END
+
+--Open up a hoping window every second and calculate the 
+--moving average humidity for the past 3 seconds.
+SELECT
+    System.Timestamp as WindowEnd,
+    avg(hmdt) as Humidity
+INTO
+    HumidityTable
+FROM
+    streamsensor
+TIMESTAMP BY
+    time
+Group by 
+    HoppingWindow(second, 3, 1);
+
+
+-------------------------------------------------------------------------------------
+-- Room Aggregations
+SELECT
+    System.timestamp as WindowEnd,
+    Dspl as device,
+    avg(temp) as temp,
+    avg(hmdt) as hmdt
+INTO
+    DojoEventHub
+FROM
+    MySensorStream
+TIMESTAMP BY
+    time
+where
+    dspl like 'YourSensorTagDsplName'
+Group by 
+    HoppingWindow(second, 3, 1), dspl
---
+++ bootcamp_new_structure/Stream_Processing_and_Event_Ingestion /SimulateTollsSourceCode/bin/Debug/Cleanup.ps1
@@ -0,0 +1,9 @@
+﻿#Cleanup Service Bus namespaces
+
+Write-Host "WARNING: This script is going to delete resources that match resource names used in the lab. Please carefully review names of the resources before confirming delete operation" -ForegroundColor Yellow
+Write-Host "Remove Service Bus namespaces starting with 'TollData'"
+Get-AzureSBNamespace | Where-Object {$_.Name -like '*TollData*'} | Remove-AzureSBNamespace -Confirm
+
+Write-Host "Remove Azure SQL servers with Administrator Login 'tolladmin'"
+Get-AzureSqlDatabaseServer | Where-Object {$_.AdministratorLogin -eq 'tolladmin'} | Remove-AzureSqlDatabaseServer -Confirm
+
