1dc24a32be6bdeba08dd0408d0e26e0d176b42eb
Phuc H Duong <phuchduong@datasciencedojo.com>
2015-08-20 16:10:46 +0100

    random forest python code for iris

---
+++ Ensemble_Methods/Code/Python-RandomForest-Iris.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import pandas
+import numpy
+
+# Load iris class into memory
+iris = load_iris() 
+
+# gaining information about the dataset
+print iris['DESCR']
+
+# loads the iris dataset into a data frame (pandas)
+# inserts column names into the data frame
+irisDF = pandas.DataFrame(iris.data, columns=iris.feature_names)
+irisDF['is_train'] = numpy.random.uniform(0, 1, len(irisDF)) <= .75
+irisDF['species'] = pandas.Categorical.from_codes(iris.target, iris.target_names)
+irisDF.head()
+
+irisDF_train = irisDF[irisDF['is_train']==True]
+irisDF_test = irisDF[irisDF['is_train']==False]
+
+features = irisDF.columnsn[:4]
+clf = RandomForestClassifier(n_jobs=2)
+y, _ = pandas.factorize(train['species'])
+clf.fit(train[features], y)
+
+preds = iris.target_names[clf.predict(test[features])]
+pandas.crosstab(test['species'], preds, rownames=['actual'], colnames=['preds'])
