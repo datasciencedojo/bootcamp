24fb71e30f06a0735cd4d34f77d1cac083a61e7f
Brian Johnson <BJOHN31595@gmail.com>
2015-09-03 15:42:27 -0700

    Improvements to Decision Tree Kyphosis lab

--- Decision_Tree/Code/Decision_Tree_Kyphosis_Data.R
+++ Decision_Tree/Code/Decision_Tree_Kyphosis_Data.R
@@ -16,27 +16,35 @@ data(kyphosis)
 ## explore the data set
 str(kyphosis)
 dim(kyphosis)
+summary(kyphosis)
 
 ## BUILD MODEL 1
 ## randomly choose 60% of the data set as training data
-set.seed(17)
-random.rows.train <- sample(1:nrow(kyphosis), 0.6*nrow(kyphosis), replace=F)
+set.seed(102)
+random.rows.train <- sample(1:nrow(kyphosis), 0.6*nrow(kyphosis))
 kyphosis.train <- kyphosis[random.rows.train,]
 dim(kyphosis.train)
 ## select the other 40% as the testing data
 random.rows.test <- setdiff(1:nrow(kyphosis),random.rows.train)
 kyphosis.test <- kyphosis[random.rows.test,]
 dim(kyphosis.test)
-## fitting decision model on training set
-set.seed(102)
-kyphosis.model1 <- rpart(Kyphosis ~ Age + Number + Start, data = kyphosis.train)
+## check testing and training set target distributions
+table(kyphosis.train$Kyphosis)
+table(kyphosis.test$Kyphosis)
+
+## fitting first decision model on training set
+kyphosis.model1 <- rpart(Kyphosis ~ ., data = kyphosis.train)
+
+## Display model
+summary(kyphosis.model1)
 
 ## MODEL 1 EVALUATION
+## Splitting based on Gini index
 ## make prediction using decision model
 kyphosis.test.predictions1 <- predict(kyphosis.model1, kyphosis.test, type = "class")
-## extract out the observations in testing set
-kyphosis.test.observations <- kyphosis.test[,1]
-## show the confusion matrix
+## extract the observations out of the  testing set
+kyphosis.test.observations <- kyphosis.test[,"Kyphosis"]
+## create confusion matrix
 confusion.matrix1 <- table(kyphosis.test.predictions1, kyphosis.test.observations)
 confusion.matrix1
 ## accuracy
@@ -53,10 +61,12 @@ F1.score1 <- 2 * precision1 * recall1 / (precision1 + recall1)
 F1.score1
 
 ## BUILD MODEL 2
-table(kyphosis.train$Kyphosis)
+## Splitting based on information gain
 ## fitting decision model on training set
-set.seed(102)
-kyphosis.model2 <- rpart(Kyphosis ~ Age + Number + Start, data = kyphosis.train, parms = list(split = "information"))
+kyphosis.model2 <- rpart(Kyphosis ~ ., data = kyphosis.train, parms = list(split = "information"))
+
+## Display model
+summary(kyphosis.model1)
 
 ## MODEL 2 EVALUATION
 ## make prediction using decision model
@@ -77,10 +87,13 @@ recall2
 F1.score2 <- 2 * precision2 * recall2 / (precision2 + recall2)
 F1.score2
 
+## Display model
+summary(kyphosis.model1)
+
 ## BUILD MODEL 3
+## Splitting on gini index; split only if node has > 10 objects, more strict split improvement requirements
 ## fitting decision model on training set
-set.seed(102)
-kyphosis.model3 <- rpart(Kyphosis ~ Age + Number + Start, data = kyphosis.train, control = rpart.control(cp = 0.1, minsplit = 10))
+kyphosis.model3 <- rpart(Kyphosis ~ ., data = kyphosis.train, control = rpart.control(cp = 0.1, minsplit = 10))
 
 ## MODEL 3 EVALUATION
 ## make prediction using decision model
@@ -117,4 +130,5 @@ text(kyphosis.model3, use.n = TRUE)
 ## Several parameters of rpart function can be used to tune the tree. Some parameters used in the above 3 models are:
 ## parms = list(split = "information"))
 ## rpart.control(cp = 0.1, minsplit = 10)
-## Check out the mannual of rpart at http://cran.r-project.org/web/packages/rpart/rpart.pdf. Fine-tune the paramters, could you build a better decision tree model with less error?
+## Check out the mannual of rpart with ?rpart.
+## Fine-tune the parameters, could you build a better decision tree model with less error?
