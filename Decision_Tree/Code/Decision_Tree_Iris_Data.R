e74d58f9921b7453b520b15d38bd4ab2825ab410
Brian Johnson <BJOHN31595@gmail.com>
2015-09-03 15:49:30 -0700

    Improvements to Decision Tree Iris lab

--- Decision_Tree/Code/Decision_Tree_Iris_Data.R
+++ Decision_Tree/Code/Decision_Tree_Iris_Data.R
@@ -17,14 +17,15 @@ data(iris)
 ## explore the data set
 str(iris)
 dim(iris)
+summary(iris)
 
 ## BUILD MODEL
-## randomly choose 1/2 of the data set as training data
-set.seed(777)
-random.rows.train <- sample(1:nrow(iris), 0.5*nrow(iris), replace=F)
+## randomly choose 70% of the data set as training data
+set.seed(27)
+random.rows.train <- sample(1:nrow(iris), 0.7*nrow(iris), replace=F)
 iris.train <- iris[random.rows.train,]
 dim(iris.train)
-## select the other 1/2 left as the testing data
+## select the other 30% left as the testing data
 random.rows.test <- setdiff(1:nrow(iris),random.rows.train)
 iris.test <- iris[random.rows.test,]
 dim(iris.test)
@@ -36,6 +37,8 @@ iris.model <- rpart(Species~., data = iris.train)
 plot(iris.model)
 title(main = "Decision Tree Model of Iris Data")
 text(iris.model, use.n = TRUE)
+## print the tree structure
+summary(iris.model)
 
 ## MODEL EVALUATION
 ## make prediction using decision model
@@ -45,10 +48,26 @@ iris.test.observations <- iris.test$Species
 ## show the confusion matrix
 confusion.matrix <- table(iris.test.predictions, iris.test.observations)
 confusion.matrix
-## calculate the accuracy in testing set
+## calculate accuracy, precision, recall, F1 for predictions
 accuracy <- sum(diag(confusion.matrix)) / sum(confusion.matrix)
 accuracy
 
+precision <- confusion.matrix1[2,2] / sum(confusion.matrix1[2,])
+precision
+
+recall <- confusion.matrix1[2,2] / sum(confusion.matrix1[,2])
+recall
+
+F1.score <- 2 * precision1 * recall1 / (precision1 + recall1)
+F1.score
+
 ## EXERCISE
-## Besides the rpart library, another library called "party" can be also used to build decision trees. It provides nonparametric regression trees for nominal, ordinal, numeric, censored, and multivariate responses. Tree growth is based on statistical stopping rules, so pruning should not be required. Mannual of party library: http://cran.r-project.org/web/packages/party/party.pdf. 
-## Instead of rpart(), try to use ctree() in "party" for the same data. They implement different algorithm when building the trees. But for this small iris data, would these two functions (with two algorithms) give us the same tree?
+## Another library called "party" can be also used to build decision trees.
+## It provides nonparametric regression trees for nominal, ordinal,
+## numeric, censored, and multivariate responses. Tree growth is based on statistical 
+## stopping rules, so pruning should not be required. 
+## party manual: http://cran.r-project.org/web/packages/party/party.pdf or ?party 
+## Instead of rpart(), try to use ctree() in "party" for the same data. 
+## They implement a different algorithm for building the tree. 
+## But for this small iris data, do these different functions (with different algorithms) 
+## give us the same tree?
