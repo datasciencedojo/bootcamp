d4a2618c19e66281753c7cefbf43b3e9fb77dd96
Brian Johnson <BJOHN31595@gmail.com>
2015-09-21 14:17:36 -0700

    Updated Logistic Regression slide deck

---
+++ R package list.R
@@ -0,0 +1,6 @@
+# This installs all packages that are used in the slides and code examples
+# There are also many dependencies, which R will automatically install
+install.packages(c("miscTools", "animation", "lattice", "rpart", "party",
+                   "randomForest", "bst", "AUC", "pROC", "e1071", "klaR",
+                   "ElemStatLearn", "glmnet", "Metrics", "fpc", "ggplot2",
+                   "caret"))
